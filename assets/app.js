a24d4fa07a514cdc0923a010b273c50b7d143c90
dovilepalavinskaite <palavinskaite.dovile@gmail.com>
2018-05-09 10:57:13 +0300

    Fixed error. Easy and Hard buttons are now working

--- assets/app.js
+++ assets/app.js
@@ -11,9 +11,36 @@ var easyBtn = document.getElementById("easyBtn");
 
 colorDisplay.textContent = pickedColor;
 
-
 // EASY AND HARD LEVEL 
 
+easyBtn.addEventListener("click", function(){
+	easyBtn.classList.add("selected");
+	hardBtn.classList.remove("selected");
+	numSquares = 3;
+	colors = generateRandomColors(numSquares);
+	pickedColor = pickColor();
+	colorDisplay.textContent = pickedColor;
+	for (var i = 0; i<squares.length; i++){
+		if (colors[i]){
+			squares[i].style.backgroundColor = colors[i];
+		} else {
+			squares[i].style.display = "none";
+		}
+	}
+})
+
+hardBtn.addEventListener("click", function(){
+	hardBtn.classList.add("selected");
+	easyBtn.classList.remove("selected");
+	numSquares = 6
+	colors = generateRandomColors(numSquares);
+	pickedColor = pickColor();
+	colorDisplay.textContent = pickedColor;
+	for (var i = 0; i<squares.length; i++){
+		squares[i].style.backgroundColor = colors[i];
+		squares[i].style.display = "block";
+	}
+})
 
 // New colors button
 
@@ -88,3 +115,5 @@ function randomColor (){
 	var b = Math.floor(Math.random() * 256);
 	return "rgb(" + r +", " + g + ", " + b +")";
 }
+
+
